b2b8e6e0ece8eacd7f17b82d2f2f6589ca9fa079
baturinasofia <sofi1608@bk.ru>
2025-10-05 13:56:56 +0500

    Task4

---
+++ Tyuiu.BaturinaSA.Sprint1.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BaturinaSA.Sprint1.Task4.V11.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint1.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 1.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0.407, res);
+        }
+    }
+}
---
+++ Tyuiu.BaturinaSA.Sprint1.Task4.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Tyuiu.BaturinaSA.Sprint1.Task4.V11.Lib;
+
+namespace Tyuiu.BaturinaSA.Sprint1.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнила: Батурина С. А. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Батурина Софья Алексеевна | ИСТНб-25-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле и печатает его на экране. Ответ          *");
+            Console.WriteLine("* округлите до 3 знаков после запятой.                                    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("arctg(x) / e^y = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
